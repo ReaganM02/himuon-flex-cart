68944c59eded65fb8a13ab6e0eb042e7932c9f0f
Reagan Mahinay <reaganmahinay.web@gmail.com>
2026-02-07 02:14:59 +0800

    Fixed max value of the cart item quantity

--- templates/cart-item.php
+++ templates/cart-item.php
@@ -11,6 +11,7 @@ $itemQuantity = (int) $cartItem['quantity'];
 $maxQuantity = $product->get_max_purchase_quantity();
 $minQuantity = $product->get_min_purchase_quantity();
 
+
 $selectedVariations = [];
 $displayVariations = [];
 $dataAttributesMap = [];
@@ -62,78 +63,86 @@ if (!empty($selectedVariations)) {
     }
 }
 ?>
-<li class="himuon-cart--item">
-    <div class="himuon-cart--media">
-        <?php if ($productPermalink): ?>
-            <a href="<?php echo esc_url($productPermalink); ?>"
-               class="himuon-cart--thumb">
-                <?php echo wp_kses_post($thumbnail); ?>
-            </a>
-        <?php else: ?>
-            <span class="himuon-cart--thumb">
-                <?php echo wp_kses_post($thumbnail); ?>
-            </span>
-        <?php endif; ?>
-    </div>
-    <div class="himuon-cart--item-content">
-        <div class="himuon-cart--item-data">
+<li class="himuon-cart--cart-item">
+    <div  class="himuon-cart--item">
+        <div class="himuon-cart--media">
             <?php if ($productPermalink): ?>
-                <a class="himuon-cart--name"
-                   href="<?php echo esc_url($productPermalink); ?>">
-                    <?php echo esc_html($productTitle); ?>
+                <a href="<?php echo esc_url($productPermalink); ?>"
+                   class="himuon-cart--thumb">
+                    <?php echo wp_kses_post($thumbnail); ?>
                 </a>
             <?php else: ?>
-                <span class="himuon-cart--name"><?php echo esc_html($productTitle); ?></span>
+                <span class="himuon-cart--thumb">
+                    <?php echo wp_kses_post($thumbnail); ?>
+                </span>
             <?php endif; ?>
-            <?php if (!empty($displayVariations)): ?>
-                <div class="himuon-cart--variations"
-                     data-product-id="<?php echo absint($product->get_parent_id()) ?>"
-                     <?php echo $variationDataAttributes; ?>
-                     data-cart-item-key="<?php echo esc_attr($cartItemKey) ?>"
-                     >
-                    <?php foreach ($displayVariations as $variation): ?>
-                        <div class="himuon-cart--variation-data">
-                            <span class="himuon-cart--variation-label">
-                                <?php echo esc_html($variation['label']); ?>:
-                            </span>
-                            <span class="himuon-cart--variation-value">
-                                <?php echo esc_html($variation['value']); ?>
-                            </span>
+        </div>
+        <div class="himuon-cart--item-content">
+            <div class="himuon-cart--item-data">
+                <?php if ($productPermalink): ?>
+                    <a class="himuon-cart--name"
+                       href="<?php echo esc_url($productPermalink); ?>">
+                        <?php echo esc_html($productTitle); ?>
+                    </a>
+                <?php else: ?>
+                    <span class="himuon-cart--name"><?php echo esc_html($productTitle); ?></span>
+                <?php endif; ?>
+                <?php if (!empty($displayVariations)): ?>
+                    <div class="himuon-cart--variations"
+                         data-product-id="<?php echo absint($product->get_parent_id()) ?>"
+                         <?php echo $variationDataAttributes; ?>
+                         data-cart-item-key="<?php echo esc_attr($cartItemKey) ?>">
+                        <?php foreach ($displayVariations as $variation): ?>
+                            <div class="himuon-cart--variation-data">
+                                <span class="himuon-cart--variation-label">
+                                    <?php echo esc_html($variation['label']); ?>:
+                                </span>
+                                <span class="himuon-cart--variation-value">
+                                    <?php echo esc_html($variation['value']); ?>
+                                </span>
+                            </div>
+                        <?php endforeach; ?>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi-chevron-right bi" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
+                            </svg>
                         </div>
-                    <?php endforeach; ?>
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                             width="12"
-                             height="12"
-                             fill="currentColor"
-                             class="bi-arrow-right bi"
-                             viewBox="0 0 16 16">
-                            <path fill-rule="evenodd"
-                                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
-                        </svg>
                     </div>
+                <?php endif; ?>
+            </div>
+            <div class="himuon-cart--right-content">
+                <div class="himuon-cart--quantity"
+                     data-cart-item-key="<?php echo esc_attr($cartItemKey); ?>">
+                    <button type="button"
+                            aria-label="<?php echo esc_attr__('Decrease quantity', 'himuon-flex-cart'); ?>"
+                            class="himuon-cart--minus">
+                        -
+                    </button>
+                    <input type="number"
+                           class="himuon-cart--qty"
+                           value="<?php echo absint($itemQuantity) ?>"
+                           min="<?php echo absint($minQuantity) ?>"
+                           <?php echo ($maxQuantity > 0) ? "max=\"$maxQuantity\"" : '' ?> />
+                    <button type="button"
+                            class="himuon-cart--plus"
+                            aria-label="<?php echo esc_attr__('Increase quantity', 'himuon-flex-cart'); ?>">+</button>
                 </div>
-            <?php endif; ?>
-        </div>
-        <div class="himuon-cart--right-content">
-            <div class="himuon-cart--quantity"
-                 data-cart-item-key="<?php echo esc_attr($cartItemKey); ?>">
-                <button type="button"
-                        aria-label="<?php echo esc_attr__('Decrease quantity', 'himuon-flex-cart'); ?>"
-                        class="himuon-cart--minus">
-                    -
-                </button>
-                <input type="number"
-                       class="himuon-cart--qty"
-                       value="<?php echo absint($itemQuantity) ?>"
-                       min="<?php echo absint($minQuantity) ?>"
-                       max="<?php echo absint($maxQuantity) ?>" />
-                <button type="button"
-                        class="himuon-cart--plus"
-                        aria-label="<?php echo esc_attr__('Increase quantity', 'himuon-flex-cart'); ?>">+</button>
-            </div>
-            <div class="himuon-cart--price">
-                <?php echo wp_kses_post(WC()->cart->get_product_price($product)); ?>
+                <div class="himuon-cart--price">
+                    <?php echo wp_kses_post(WC()->cart->get_product_price($product)); ?>
+                </div>
             </div>
         </div>
+    </div>
+    <div class="himuon-cart--cart-item-action">
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi-chevron-right bi" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
+        </svg>
+    </div>
+    <div class="himuon-cart--actions">
+       <div class="himuon-cart--actions-content">
+         <div class="himuon-cart--action-delete" data-cart-item-key="<?php echo esc_attr($cartItemKey)?>">
+            <?php echo esc_html__('Delete', 'himuon-flex-cart') ?>
+        </div>
+       </div>
+    </div>
 </li>
