9f6f3a480bac9ef572a6a93f40e53a493f81e391
Reagan Mahinay <reaganmahinay.web@gmail.com>
2026-02-12 03:18:19 +0800

    Updated the cart item panel to improve maintainability

--- templates/edit-cart-item.php
+++ templates/edit-cart-item.php
@@ -1,4 +1,5 @@
 <?php
+
 // Exit if accessed directly.
 if (!defined('ABSPATH')) {
     exit;
@@ -10,17 +11,21 @@ $args = [
 ];
 ?>
 <div class="himuon-cart--form-wrapper">
-    <div class="himuon-cart--variation-product-title"><?php echo esc_attr($data['product']->get_name()); ?></div>
+    <div class="himuon-cart--variation-product-title">
+        <?php echo esc_html($data['title']); ?>
+    </div>
     <?php wc_get_template('single-product/add-to-cart/variable.php', $args); ?>
 </div>
 <div class="himuon-cart--variation-action">
-    <a class="himuon-cart--variation-link"
-       href="<?php echo get_permalink($productID) ?>">
-        <?php echo esc_html__('View Product', 'himuon-flex-cart'); ?>
-    </a>
+    <?php if (!empty($data['permalink'])): ?>
+        <a class="himuon-cart--variation-link"
+           href="<?php echo esc_url($data['permalink']); ?>">
+            <?php echo esc_html($data['viewLabel']) ?>
+        </a>
+    <?php endif; ?>
     <button type="button"
             class="himuon-cart--variation-update-cart-item"
             data-cart-item-key="<?php echo esc_attr($data['cartItemKey']) ?>">
-        <?php echo esc_html__('Update Item', 'himuon-flex-cart'); ?>
+        <?php echo esc_html($data['updateLabel']); ?>
     </button>
 </div>
