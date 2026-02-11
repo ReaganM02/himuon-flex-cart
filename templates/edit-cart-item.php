0a9ca33beb558224d16f3e44017cb6decc45053b
Reagan Mahinay <reaganmahinay.web@gmail.com>
2026-02-11 14:46:28 +0800

    Updated edit cart item content

--- templates/edit-cart-item.php
+++ templates/edit-cart-item.php
@@ -4,12 +4,13 @@ if (!defined('ABSPATH')) {
     exit;
 }
 $args = [
-    'available_variations' => $availableVariations,
-    'attributes' => $attributes,
-]
-    ?>
+    'product' => $data['product'],
+    'attributes' => $data['attributes'],
+    'available_variations' => $data['available_variations'],
+];
+?>
 <div class="himuon-cart--form-wrapper">
-    <div class="himuon-cart--variation-product-title"><?php echo $product->get_name() ?></div>
+    <div class="himuon-cart--variation-product-title"><?php echo esc_attr($data['product']->get_name()); ?></div>
     <?php wc_get_template('single-product/add-to-cart/variable.php', $args); ?>
 </div>
 <div class="himuon-cart--variation-action">
@@ -18,7 +19,8 @@ $args = [
         <?php echo esc_html__('View Product', 'himuon-flex-cart'); ?>
     </a>
     <button type="button"
-            class="himuon-cart--variation-update-cart-item">
+            class="himuon-cart--variation-update-cart-item"
+            data-cart-item-key="<?php echo esc_attr($data['cartItemKey']) ?>">
         <?php echo esc_html__('Update Item', 'himuon-flex-cart'); ?>
     </button>
 </div>
