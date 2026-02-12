c3957e345dd6910276cf15bdc1bba8e7a548034b
Reagan Mahinay <reaganmahinay.web@gmail.com>
2026-02-12 12:40:48 +0800

    Extended hooks and APIs

--- src/Frontend/EditCartItemView.php
+++ src/Frontend/EditCartItemView.php
@@ -33,8 +33,32 @@ final class EditCartItemView
             'permalink' => CartItemView::permalink($cartItemKey, $cartItem, $parent),
             'attributes' => $parent->get_variation_attributes(),
             'available_variations' => $parent->get_available_variations(),
+            'initialImage' => self::initialImageData($product, $parent),
             'viewLabel' => self::viewProductLabel($cartItem, $cartItemKey, $product),
-            'updateLabel' => self::updateCartLabel($cartItem, $cartItemKey, $product)
+            'updateLabel' => self::updateCartLabel($cartItem, $cartItemKey, $product),
+            'closeEditPanel' => self::closeEditPanel($cartItem, $cartItemKey, $product)
+        ];
+    }
+
+    private static function initialImageData(WC_Product $product, WC_Product_Variable $parent)
+    {
+        $imageProduct = $product->get_image_id() ? $product : $parent;
+        $imageId = (int) $imageProduct->get_image_id();
+
+        if (!$imageId) {
+            return [
+                'src' => wc_placeholder_img_src('woocommerce_thumbnail'),
+                'srcset' => '',
+                'sizes' => '',
+                'alt' => $imageProduct->get_name(),
+            ];
+        }
+
+        return [
+            'src' => wp_get_attachment_image_url($imageId, 'woocommerce_thumbnail'),
+            'srcset' => wp_get_attachment_image_srcset($imageId, 'woocommerce_thumbnail'),
+            'sizes' => wp_get_attachment_image_sizes($imageId, 'woocommerce_thumbnail'),
+            'alt' => get_post_meta($imageId, '_wp_attachment_image_alt', true) ?: $imageProduct->get_name(),
         ];
     }
 
@@ -66,6 +90,20 @@ final class EditCartItemView
         return (string) apply_filters(
             'himuon_flex_cart_edit_cart_item_update_action',
             __('Update', 'himuon-flex-cart'),
+            $cartItem,
+            $cartItemKey,
+            $product
+        );
+    }
+
+    private static function closeEditPanel(array $cartItem, string $cartItemKey, WC_Product $product)
+    {
+        $svg = '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+        </svg>';
+        return (string) apply_filters(
+            'himuon_flex_cart_close_edit_panel_icon',
+            $svg,
             $cartItem,
             $cartItemKey,
             $product
