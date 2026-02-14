113c0c8c7c0e99c3a3a45f1887176814d05db702
Reagan Mahinay <reaganmahinay.web@gmail.com>
2026-02-14 17:12:11 +0800

    Improve, Fix bug, and added gurads to prevent any issues during cart adding.

--- assets/js/himuon-flex-cart-add-to-cart.js
+++ assets/js/himuon-flex-cart-add-to-cart.js
@@ -16,19 +16,34 @@ jQuery(function ($) {
 
         $button.addClass('loading').prop('disabled', true)
 
-        const dataArray = $form.serializeArray()
+        let dataArray = $form.serializeArray()
+        dataArray = dataArray.filter(item => item.name !== 'add-to-cart')
+
         const hasProductId = dataArray.some(function (field) {
             return field.name === 'product_id'
         })
 
         const hasButtonValue = $button.length && String($button.val() || '').trim() !== ''
 
+        // For Simple Product
         if (!hasProductId && hasButtonValue) {
             const buttonValue = String($button.val() || '').trim()
             dataArray.push({ name: 'product_id', value: buttonValue })
         }
 
+        const variationField = dataArray.find((field) => field.name === 'variation_id')
+        const variationId = variationField ? parseInt(variationField.value, 10) : 0
+
+        // For Variable Products
+        if ($form.hasClass('variations_form') && variationId > 0) {
+            const productID = dataArray.find((item) => item.name === 'product_id')
+            if (productID) {
+                productID.value = variationId
+            }
+        }
+
         const data = $.param(dataArray)
+
         $(document.body).trigger('adding_to_cart', [$button, data])
 
         $.ajax({
@@ -43,6 +58,12 @@ jQuery(function ($) {
             if (!response) {
                 return
             }
+
+            if (response.error && response.product_url) {
+                window.location = response.product_url
+                return
+            }
+
             $(document.body).trigger('added_to_cart', [response.fragments || {}, response.cart_hash || '', $button])
         }).always(function () {
             $button.removeClass('loading').prop('disabled', false)
